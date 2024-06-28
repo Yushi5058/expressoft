f52a3c8fac2e4f42b6d55cef946635384e449e13
Yushi <yushi_61@proton.me>
2024-06-28 22:00:25 +0100

    fix: zsh completion color for readability

--- wezterm/expressoft.lua
+++ wezterm/expressoft.lua
@@ -1,19 +1,20 @@
 -- Colorscheme : Expressoft
 -- Author : Yushi5058
 return {
-	color_scheme = "expressoft",
-	color_schemes = {
-		["expressoft"] = {
-			foreground = "#aa998b",
-			background = "#120d09",
-			cursor_bg = "#cabeb3",
-			cursor_border = "#766454",
-			cursor_fg = "#241b13",
-			selection_bg = "#766454",
-			selection_fg = "#8f7c6d",
+    color_scheme = "expressoft",
+    window_background_opacity = 0.7,
+    color_schemes = {
+        ["expressoft"] = {
+            foreground = "#aa998b",
+            background = "#120d09",
+            cursor_bg = "#cabeb3",
+            cursor_border = "#766454",
+            cursor_fg = "#241b13",
+            selection_bg = "#766454",
+            selection_fg = "#8f7c6d",
 
 			ansi = { "#120d09", "#9c4473", "#739c44", "#9c6d44", "#44739c", "#6d449c", "#449c6d", "#4b3c2e" },
-			brights = {  "#120d09", "#9c4473", "#739c44", "#9c6d44", "#44739c", "#6d449c", "#449c6d", "#4b3c2e"},
+			brights = {  "#8f7c6d", "#9c4473", "#739c44", "#9c6d44", "#44739c", "#6d449c", "#449c6d", "#4b3c2e"},
 		},
 	},
 }
